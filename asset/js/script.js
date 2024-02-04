615f68e0a070a5ca559770d2477bd6f9f660ac1f
piya-ds <86950823+piya-ds@users.noreply.github.com>
2024-02-04 13:11:15 +0000

    add content load and click event listener

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,3 +1,25 @@
+// Add event listener when the page load
+//add event listener when button is clicked
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    let buttons = document.getElementsByTagName('button');
+
+    for( let button of buttons){
+        button.addEventListener('click', function(){
+
+            if(button.getAttribute('data-type') === "submit"){
+                alert('you clicked Submit');
+            }
+            else{
+                let gameType = button.getAttribute('data-type');
+                alert(`You clicked ${gameType}`);
+            }
+        })
+        
+    }
+})
+
 function runGame() {
 
 }
